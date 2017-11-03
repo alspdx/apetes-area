42ed8db1bf78c2182eeddaa8279863fe0203edd9
Adam Smith <alspdx@gmail.com>
2017-11-03 16:11:23 -0700

    add output of items on pizza on order confirmation screen

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,3 @@
-var pizzaOrder;
-
 function Pizza (size, meats, veggies, cheeses){
   this.size = size;
   this.meats = meats;
@@ -42,6 +40,23 @@ Pizza.prototype.pizzaPrice = function() {
   return (this.basePrices() + ((this.vegPrice() + this.meatCheesePrice()) * this.pizzaToppingMultiplier()))
 }
 
+Pizza.prototype.toppingsOutput = function() {
+  var meats = this.meats;
+  var veggies = this.veggies;
+  var cheeses = this.cheeses;
+  var toppings = meats.concat(veggies.concat(cheeses));
+  var toppingsBeforeAnd = [];
+  for (i = 0; i < (toppings.length - 1); i++) {
+    toppingsBeforeAnd.push(toppings[i]);
+  }
+  var output = "You ordered a " + this.size + " size";
+  if (toppings.length > 0) {
+    output += " pizza with " + toppingsBeforeAnd.join(", ") + " and " + toppings[toppings.length - 1] + ".";
+  } else {
+    output += " plain cheese pizza."
+  }
+  return output
+}
 
 $(function() {
   $("#display-form-button").click(function() {
@@ -49,9 +64,9 @@ $(function() {
     $(".pizza-order").slideToggle();
   });
 
-
   $("#order-input").submit(function(event) {
     event.preventDefault();
+
     var sizeInput = $("#size-input input:radio:checked").val();
     var meatsInput = $("#meats-input input:checkbox:checked").map(function() {
       return $(this).val();
@@ -62,9 +77,11 @@ $(function() {
     var cheeseInput = $("#cheese-input input:checkbox:checked").map(function() {
       return $(this).val();
     }).get();
-    pizzaOrder = new Pizza(sizeInput, meatsInput, veggiesInput, cheeseInput);
-    console.log(pizzaOrder)
-    $("#order-output").text(pizzaOrder.pizzaPrice());
+
+    var pizzaOrder = new Pizza(sizeInput, meatsInput, veggiesInput, cheeseInput);
+
+    $("#order-output").text(pizzaOrder.toppingsOutput());
+    $("#price-output").text(pizzaOrder.pizzaPrice());
     $(".pizza-order").slideToggle();
     $(".pizza-recall").slideToggle();
   });
